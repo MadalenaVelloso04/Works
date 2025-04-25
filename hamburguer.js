6766ed60d2e80562f2631bf8e7cd867723d1af6d
Madalenavelloso <madalenavelloso@gmail.com>
2025-04-25 22:26:51 +0200

    PROJECT CHOURIÇO

--- hamburguer.js
+++ hamburguer.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const navLinks = document.getElementById("nav-links");
+  const navLinkItems = navLinks.querySelectorAll("a[href^='#']");
 
   if (menuToggle && navLinks) {
+    // Toggle menu open/close
     menuToggle.addEventListener("click", () => {
       navLinks.classList.toggle("show");
       menuToggle.classList.toggle("open");
+    });
+
+    // Auto-close on internal link click
+    navLinkItems.forEach(link => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+        menuToggle.classList.remove("open");
+      });
     });
   }
 });
