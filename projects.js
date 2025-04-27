5e48b8d99431d706cdf7c54b7562ffe6f93698a8
MadalenaVelloso04 <madalenavelloso04@gmail.com>
2025-04-27 10:32:45 +0200

    cards

--- projects.js
+++ projects.js
@@ -18,3 +18,12 @@ function showCategory(id) {
 document.addEventListener("DOMContentLoaded", () => {
   showCategory('Posters');
 });
+
+function openModal(src) {
+  document.getElementById('modalImage').src = src;
+  document.getElementById('imageModal').classList.remove('hidden');
+}
+
+function closeModal() {
+  document.getElementById('imageModal').classList.add('hidden');
+}
